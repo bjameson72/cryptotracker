85305b297fd9fb8da69c92c109c7c8624383596a
Brock Jameson <Bjameson72@gmail.com>
2019-11-27 16:30:32 -0800

    oops, not /dashboard

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'dR7f9tlR4JqeWEo5meV8NsxyF2di1YiA',
   domain: 'csc-csc.auth0.com',
-  callbackURL: 'https://cryptotracker360.netlify.com/dashboard'
+  callbackURL: 'https://cryptotracker360.netlify.com/'
 };
